1d72590a3118ea09bbf230bc04a68231108ddf8b
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2021-12-27 14:57:57 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import logo from "./logo.svg";
+import logo2 from "./day.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo2} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/I-keep-trying/countries3"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
